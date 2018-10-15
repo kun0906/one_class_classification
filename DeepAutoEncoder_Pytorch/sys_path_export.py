bfc68601d1b84a337996808d590f40577bf8598f
Learn-Live <kun.bj@foxmail.com>
2018-10-14 20:47:13 -0400

    add 'parent path' to system path so that the script can call 'parent directory'

--- DeepAutoEncoder_Pytorch/sys_path_export.py
+++ DeepAutoEncoder_Pytorch/sys_path_export.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     add 'parent path' to system path so that the script can call 'parent directory'
+    it is no need to do in IDE environment, however, it must be done in shell/command environment
 """
 import os, sys
 
--- OCSVM_Sklearn/main_ocsvm_case3_train_set_without_abnormal_data.py
+++ OCSVM_Sklearn/main_ocsvm_case3_train_set_without_abnormal_data.py
@@ -36,7 +36,7 @@ from sklearn import svm
 from sklearn.metrics import roc_auc_score, confusion_matrix
 from sklearn.metrics.pairwise import pairwise_distances
 
-from sys_path_export import *  # this is must be done in command environment
+from sys_path_export import *  # it is no need to do in IDE environment, however, it must be done in shell/command environment
 from Utilities.common_funcs import dump_model, load_model, achieve_train_val_test_from_files
 
 
