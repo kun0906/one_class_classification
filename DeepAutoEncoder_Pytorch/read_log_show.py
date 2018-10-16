b5cef233b58adde695a422ec673f1e80a9862951
Learn-Live <kun.bj@foxmail.com>
2018-10-16 16:08:38 -0400

    For an one-class model(OCSVM) ,  +1 (inlier) or -1 (outlier) is returned.

--- DeepAutoEncoder_Pytorch/read_log_show.py
+++ DeepAutoEncoder_Pytorch/read_log_show.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-
+    parse the out_log and show the data
 """
 from Utilities.common_funcs import show_data
 
@@ -13,7 +13,7 @@ def parse_log(input_file):
             if not line.startswith('epoch ['):
                 line = in_f.readline()
                 continue
-            line_arr = line.split(',')
+            line_arr = line.strip().split(',')
             loss['train_loss'].append(float(line_arr[1].strip().split(':')[-1]))
             loss['val_loss'].append(float(line_arr[2].strip().split(':')[-1]))
             line = in_f.readline()
@@ -24,6 +24,7 @@ def parse_log(input_file):
 if __name__ == '__main__':
     input_file = './outlog_9299217_4294967294.out'
     loss = parse_log(input_file)
-    show_data(loss['train_loss'], x_label='epochs', y_label='loss', fig_label='', title='train_loss')
-    show_data(loss['val_loss'], x_label='epochs', y_label='loss', fig_label='', title='val_loss')
-    print(loss)
+    show_data(loss['train_loss'], x_label='epochs', y_label='loss', fig_label='train_loss', title='train_loss')
+    show_data(loss['val_loss'], x_label='epochs', y_label='loss', fig_label='val_loss', title='val_loss')
+    print('train_loss:', loss['train_loss'])
+    print('val_loss:', loss['val_loss'])
